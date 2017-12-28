91e069064fc66f5eb6c1208f953616b69b5309a1
Richard Remer <rich.remer@gmail.com>
2017-12-28 11:41:40 -0800

    port from socket-server project

---
+++ safe.js
@@ -0,0 +1,83 @@
+/**
+ * Create object proxy to access properties safely.
+ * @param {object} object
+ * @returns {Proxy}
+ */
+function safe(object) {
+    const path = arguments[1] || [];
+
+    return new Proxy(object, {
+        get(target, name) {
+            switch (name) {
+                case "pop":     return pop;
+                case "push":    return push;
+                case "valueOf": return valueOf;
+                default:        return safe(object, path.concat(name));
+            }
+        },
+
+        set(target, property, value, receiver) {
+            var context = object;
+
+            path.forEach(prop => {
+                if (!(prop in context)) context[prop] = {};
+                context = context[prop];
+            });
+
+            context[property] = value;
+        }
+    });
+
+    /**
+     * Resolve the path, create missing intermediary objects, and set a fallback
+     * value if one was not already set.  Return the effective value.
+     * @param {*} value
+     * @returns {*}
+     */
+    function fallback(value) {
+        const intermediate = path.slice(0,-1);
+        const final = path[path.length-1];
+        var obj;
+
+        if (!path.length) {
+            throw new Error("setting fallback undefined for base target");
+        }
+
+        // ensure intermediate objects are set and return the final object
+        obj = intermediate.reduce((obj, prop) => {
+            if (obj[prop] === undefined) obj[prop] = {};
+            return obj[prop];
+        }, object);
+
+        // set fallback value if one is not set and return result
+        if (obj[final] === undefined) obj[final] = value;
+        return obj[final];
+    }
+
+    /**
+     * Resolve the path and attempt to pop a value from it.
+     * @returns {*}
+     */
+    function pop() {
+        const value = valueOf();
+        return value === undefined ? value : value.pop();
+    }
+
+    /**
+     * Resolve the path, fallback to an empty array, and push a value on it.
+     * @param {*} value
+     */
+    function push(value) {
+        fallback([]).push(value);
+    }
+
+    /**
+     * Resolve the path to its actual value.
+     * @returns {*}
+     */
+    function valueOf() {
+        return path.reduce((obj, prop) => obj ? obj[prop] : undefined, object);
+    }
+}
+
+module.exports = safe;
---
+++ test/test-safe.js
@@ -0,0 +1,64 @@
+const expect = require("expect.js");
+const safe = require("..");
+
+describe("safe(object)", () => {
+    var object,
+        proxy;
+
+    beforeEach(() => {
+        object = {a: 42, b: {c: "foo"}};
+        proxy = safe(object);
+    });
+
+    it("should return virtual proxy properties", () => {
+        expect(proxy.a).to.be.an("object");
+        expect(proxy.zing).to.be.an("object");
+        expect(proxy.zap).to.be.an("object");
+    });
+
+    it("should return virtual nested proxy properties", () => {
+        expect(proxy.a.b.c).to.be.an("object");
+    });
+
+    it("should update underlying object when assigned", () => {
+        proxy.zing = 42;
+        expect(object.zing).to.be(42);
+    });
+
+    it("should update nested object properties when assigned", () => {
+        proxy.b.c = "bang";
+        expect(object.b.c).to.be("bang");
+    });
+
+    it("should push value to nested array when pushed", () => {
+        object.array = [];
+        proxy.array.push("foo");
+        expect(object.array.length).to.be(1);
+        expect(object.array).to.contain("foo");
+    });
+
+    it("should pop value from nested array when popped", () => {
+        object.array = ["foo"];
+        expect(proxy.array.pop()).to.be("foo");
+        expect(object.array.length).to.be(0);
+    });
+
+    it("should generate intermediary objects as necessary", () => {
+        proxy.foo.bar.baz = 13;
+        expect(object.foo).to.be.an("object");
+        expect(object.foo.bar).to.be.an("object");
+        expect(object.foo.bar.baz).to.be(13);
+    });
+
+    it("should generate terminal array for push as necessary", () => {
+        proxy.foo.push("foo");
+        expect(object.foo).to.be.an("array");
+        expect(object.foo.length).to.be(1);
+        expect(object.foo).to.contain("foo");
+    });
+
+    it("should not generate unnecessary objects", () => {
+        expect(proxy.foo.bar).to.be.an("object");
+        expect(object.foo).to.be(undefined);
+    });
+});
